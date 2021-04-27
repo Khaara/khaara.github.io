0df2d3d33ea1b6a236e0485e4cf9b1795d8ef1f0
Bilhan <bilhanlatino@yahoo.com>
2021-04-26 18:18:35 -0600

    retorno pa continuar estructura

--- assets/js/proyecto.js
+++ assets/js/proyecto.js
@@ -1,3 +1,31 @@
-$(window).scroll(function(){
+/*$(window).scroll(function(){
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
 });
+
+
+$(window).on("scroll", function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('bg-dark');
+    }
+
+    else{
+        $('nav').removeClass('bg-dark');
+    }
+})
+
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+  
+.navbar-fixed-top.scrolled {
+  background-color: #fff !important;
+  transition: background-color 200ms linear;
+}
+  */
+
+  
